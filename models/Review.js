fe8c09cedadb3be21cd5a12e52c3f6610e69404f
sivalakshmik <soniakv.2822@gmail.com>
2025-10-26 17:24:10 +0530

    Update Review.js

--- models/Review.js
+++ models/Review.js
@@ -1,35 +1,13 @@
 import mongoose from "mongoose";
 
-const reviewSchema = new mongoose.Schema({
-  vehicle: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Vehicle",
-    required: true,
+const reviewSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    vehicle: { type: mongoose.Schema.Types.ObjectId, ref: "Vehicle", required: true },
+    rating: { type: Number, min: 1, max: 5, required: true },
+    comment: { type: String, required: true },
   },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  rating: {
-    type: Number,
-    required: true,
-    min: 1,
-    max: 5,
-  },
-  comment: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  approved: {
-    type: Boolean,
-    default: false, // For moderation
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 
 export default mongoose.model("Review", reviewSchema);
